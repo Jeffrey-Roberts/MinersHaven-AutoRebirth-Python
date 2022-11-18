52a9a6009def13ba9a0c035e70050bc24ec6f2a8
Jeffrey-Roberts <jeffreyjayroberts@gmail.com>
2022-11-17 20:16:05 -0600

    Optimized and Refactored Code

--- rebirth.py
+++ rebirth.py
@@ -1,112 +1,82 @@
-# Miners Haven Rebirth Script
-# 12/12/2020
 import pyautogui
 import time
 
+TRAVEL_TIME = 1
 
-# Moves off-screen to fix ROBLOX cursor movement
+DELAY = .10
+
+
 def fix():
-    time.sleep(0.25)
-    pyautogui.click(4036, 546)
+    time.sleep(DELAY)
+    pyautogui.click(pyautogui.size()[0]/2, pyautogui.size()[0]-10)
 
 
-# Begins rebirth setup
-def rebirth():
-
-    while 1:
-        time.sleep(1)
-        bar_found = False
-        settings_found = False
-        layout_found = False
-        yes_found = False
-
-        # Clicks settings button
-        while not settings_found:
-            img_position = pyautogui.locateCenterOnScreen('images/settings.png', grayscale=False, confidence=0.9)
-            if img_position:
-                print('settings found!')
-                time.sleep(0.15)
-                pyautogui.doubleClick(img_position)
-                settings_found = True
-                fix()
-            else:
-                print("settings not found!")
-
-        # Clicks layout button
-        img_position = pyautogui.locateCenterOnScreen('images/layout.png', grayscale=False, confidence=0.8)
-        if img_position:
-            print('layout found!')
-            time.sleep(0.15)
-            pyautogui.doubleClick(img_position)
-            fix()
-        else:
-            print("layout not found!")
-
-        # Clicks load button
-        img_position = pyautogui.locateCenterOnScreen('images/load.png', grayscale=False, confidence=0.7)
-        if img_position:
-            print('load found!')
-            time.sleep(0.15)
-            pyautogui.doubleClick(img_position)
-            fix()
-        else:
-            print("load not found!")
-
-        time.sleep(5)
-        while not layout_found:
-            img_position = pyautogui.locateCenterOnScreen('images/load.png', grayscale=False, confidence=0.7)
-            if img_position:
-                print('load found!')
-                time.sleep(0.15)
-                pyautogui.doubleClick(img_position)
-                layout_found = True
-                fix()
-            else:
-                print("load not found!")
-
-        # Clicks settings button
+def click_settings():
+    while pyautogui.locateOnScreen('images/layout.png', grayscale=False, confidence=0.8) is None:
+        time.sleep(DELAY)
         img_position = pyautogui.locateCenterOnScreen('images/settings.png', grayscale=False, confidence=0.9)
         if img_position:
             print('settings found!')
-            time.sleep(0.2)
-            pyautogui.doubleClick(img_position)
+            time.sleep(DELAY)
             fix()
+            pyautogui.doubleClick(img_position)
         else:
             print("settings not found!")
+    fix()
 
-        # Checks rebirth eligibility
-        while not bar_found:
-            img_position = pyautogui.locateCenterOnScreen('images/bar.png', grayscale=False)
-            if img_position:
-                print('bar found!')
-                time.sleep(0.15)
-                pyautogui.doubleClick(img_position)
-                bar_found = True
-                fix()
-            else:
-                print("bar not found!")
 
-        # Clicks rebirth button
-        img_position = pyautogui.locateCenterOnScreen('images/rebirth.png', grayscale=False, confidence=0.7)
+def click_layouts():
+    while pyautogui.locateCenterOnScreen('images/load.png', grayscale=False, confidence=0.7) is None:
+        time.sleep(DELAY)
+        img_position = pyautogui.locateCenterOnScreen('images/layout.png', grayscale=False, confidence=0.8)
         if img_position:
-            print('rebirth found!')
-            time.sleep(.5)
+            print('layout found!')
+            time.sleep(DELAY)
+            pyautogui.doubleClick(img_position)
+        else:
+            print("layout not found!")
+    fix()
+
+
+def load_layout():
+    load_found = False
+    first_load = False
+    while not load_found:
+        time.sleep(DELAY)
+        img_position = pyautogui.locateCenterOnScreen('images/load.png', grayscale=False, confidence=0.7)
+        if img_position and not first_load:
+            print('load found!')
+            # pyautogui.doubleClick(img_position)
+            pyautogui.doubleClick(x=1521, y=650)
+            first_load = True
+            fix()
+        elif pyautogui.locateOnScreen('images/money.png') is None:
+            pyautogui.doubleClick(x=1521, y=650)
+            load_found = True
+        else:
+            print("load not found!")
+    fix()
+
+
+def click_rebirth():
+    while pyautogui.locateCenterOnScreen('images/yes2.png', grayscale=False, confidence=0.8) is None:
+        time.sleep(DELAY)
+        img_position = pyautogui.locateCenterOnScreen('images/rebirth.png', grayscale=False, confidence=0.7)
+        if pyautogui.locateOnScreen('images/bar.png') is not None:
+            print("bar completed")
             pyautogui.doubleClick(img_position)
             fix()
         else:
-            print("rebirth not found!")
-
-        # Clicks yes button
-        while not yes_found:
-            img_position = pyautogui.locateCenterOnScreen('images/yes2.png', grayscale=False, confidence=0.8)
-            if img_position:
-                print('yes found!')
-                time.sleep(0.25)
-                pyautogui.doubleClick(img_position)
-                yes_found = True
-                fix()
-            else:
-                print("yes not found!")
+            print("bar not completed")
+    img_position = pyautogui.locateCenterOnScreen('images/yes2.png', grayscale=False, confidence=0.8)
+    pyautogui.doubleClick(img_position)
+    fix()
 
 
-rebirth()
+
+time.sleep(1)
+click_settings()
+click_layouts()
+load_layout()
+click_settings()
+click_rebirth()
