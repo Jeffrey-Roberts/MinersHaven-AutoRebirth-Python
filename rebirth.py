1d7bdb6732192fbed5927e0a3c86b70e13c74ca0
Jeffrey-Roberts <jeffreyjayroberts@gmail.com>
2022-11-19 11:13:38 -0600

    Adjusted fix() locations

--- rebirth.py
+++ rebirth.py
@@ -19,10 +19,12 @@ def pause():
 
     while paused:
         time.sleep(1)
+    fix()
 
 
 def click_settings():
     global paused
+    fix()
     while pyautogui.locateOnScreen('images/layout.png', grayscale=False, confidence=0.8) is None:
         time.sleep(DELAY)
         img_position = pyautogui.locateCenterOnScreen('images/settings.png', grayscale=False, confidence=0.9)
@@ -34,11 +36,11 @@ def click_settings():
         else:
             pause() if paused else None
             print("settings not found!")
-    fix()
 
 
 def click_layouts():
     global paused
+    fix()
     while pyautogui.locateCenterOnScreen('images/load.png', grayscale=False, confidence=0.7) is None:
         time.sleep(DELAY)
         img_position = pyautogui.locateCenterOnScreen('images/layout.png', grayscale=False, confidence=0.8)
@@ -49,13 +51,13 @@ def click_layouts():
         else:
             pause() if paused else None
             print("layout not found!")
-    fix()
 
 
 def load_layout():
     global paused
     load_found = False
     first_load = False
+    fix()
     while not load_found:
         time.sleep(DELAY)
         img_position = pyautogui.locateCenterOnScreen('images/load.png', grayscale=False, confidence=0.7)
@@ -64,33 +66,31 @@ def load_layout():
             # pyautogui.doubleClick(img_position)
             pyautogui.doubleClick(x=1521, y=650)
             first_load = True
-            fix()
         elif pyautogui.locateOnScreen('images/money.png') is None:
+            fix()
             pyautogui.doubleClick(x=1521, y=650)
             load_found = True
         else:
             pause() if paused else None
             print("load not found!")
-    fix()
 
 
 def click_rebirth():
     global paused
+    fix()
     while pyautogui.locateCenterOnScreen('images/yes2.png', grayscale=False, confidence=0.8) is None:
         time.sleep(DELAY)
         img_position = pyautogui.locateCenterOnScreen('images/rebirth.png', grayscale=False, confidence=0.6)
         if pyautogui.locateOnScreen('images/bar.png') is not None:
             print("bar completed")
             pyautogui.doubleClick(img_position)
-            fix()
         else:
             pause() if paused else None
             print("bar not completed")
-
+    fix()
     while pyautogui.locateCenterOnScreen('images/yes2.png', grayscale=False, confidence=0.8) is not None:
         img_position = pyautogui.locateCenterOnScreen('images/yes2.png', grayscale=False, confidence=0.8)
         pyautogui.doubleClick(img_position)
-    fix()
 
 
 def function():
