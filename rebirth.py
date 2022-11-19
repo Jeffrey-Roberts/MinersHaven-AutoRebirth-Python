ada4f529443d9a8f311e943a298dda4ed60de658
Jeffrey-Roberts <jeffreyjayroberts@gmail.com>
2022-11-19 10:22:13 -0600

    Changed pause locations

--- rebirth.py
+++ rebirth.py
@@ -6,13 +6,23 @@ from pynput.keyboard import Key, Listener
 TRAVEL_TIME = 1
 DELAY = .10
 
+paused = False
+
 
 def fix():
     time.sleep(DELAY)
     pyautogui.click(pyautogui.size()[0]/2, pyautogui.size()[0]-10)
 
 
+def pause():
+    global paused
+
+    while paused:
+        time.sleep(1)
+
+
 def click_settings():
+    global paused
     while pyautogui.locateOnScreen('images/layout.png', grayscale=False, confidence=0.8) is None:
         time.sleep(DELAY)
         img_position = pyautogui.locateCenterOnScreen('images/settings.png', grayscale=False, confidence=0.9)
@@ -22,11 +32,13 @@ def click_settings():
             fix()
             pyautogui.doubleClick(img_position)
         else:
+            pause() if paused else None
             print("settings not found!")
     fix()
 
 
 def click_layouts():
+    global paused
     while pyautogui.locateCenterOnScreen('images/load.png', grayscale=False, confidence=0.7) is None:
         time.sleep(DELAY)
         img_position = pyautogui.locateCenterOnScreen('images/layout.png', grayscale=False, confidence=0.8)
@@ -35,11 +47,13 @@ def click_layouts():
             time.sleep(DELAY)
             pyautogui.doubleClick(img_position)
         else:
+            pause() if paused else None
             print("layout not found!")
     fix()
 
 
 def load_layout():
+    global paused
     load_found = False
     first_load = False
     while not load_found:
@@ -55,11 +69,13 @@ def load_layout():
             pyautogui.doubleClick(x=1521, y=650)
             load_found = True
         else:
+            pause() if paused else None
             print("load not found!")
     fix()
 
 
 def click_rebirth():
+    global paused
     while pyautogui.locateCenterOnScreen('images/yes2.png', grayscale=False, confidence=0.8) is None:
         time.sleep(DELAY)
         img_position = pyautogui.locateCenterOnScreen('images/rebirth.png', grayscale=False, confidence=0.6)
@@ -68,13 +84,11 @@ def click_rebirth():
             pyautogui.doubleClick(img_position)
             fix()
         else:
+            pause() if paused else None
             print("bar not completed")
     img_position = pyautogui.locateCenterOnScreen('images/yes2.png', grayscale=False, confidence=0.8)
     pyautogui.doubleClick(img_position)
     fix()
-
-
-paused = False
 
 
 def function():
@@ -82,29 +96,10 @@ def function():
 
     while True:
         time.sleep(1)
-
-        while paused:
-            time.sleep(1)
-
         click_settings()
-        while paused:
-            time.sleep(1)
-
         click_layouts()
-
-        while paused:
-            time.sleep(1)
-
         load_layout()
-
-        while paused:
-            time.sleep(1)
-
         click_settings()
-
-        while paused:
-            time.sleep(1)
-
         click_rebirth()
 
 
@@ -116,8 +111,6 @@ def on_release(key):
 
     if str(key).strip('\'') == 'k':
         paused = not paused
-
-
 
 
 thread = threading.Thread(target=function)
